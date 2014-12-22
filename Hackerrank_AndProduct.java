5389edf5a7423390a6a2ccd05200afb3e1c73713
licong1112 <licong1112@gmail.com>
2014-12-21 20:23:48 -0500

    Minor changes

--- Hackerrank_AndProduct.java
+++ Hackerrank_AndProduct.java
@@ -2,7 +2,7 @@ package group_practice;
 
 public class Hackerrank_AndProduct {
 	public static long myMethod(long a, long b) {
-		int i = 31;
+		int i = 32;
         long result = 0;
         while (((a >> i) & 1) == ((b >> i) & 1) && i >= 0) {
         	if (((a >> i) & 1) == 1) {
